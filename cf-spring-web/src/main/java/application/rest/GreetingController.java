8297d8aaaf43545b7416113d104cbc503e6d2dc5
Michael C. Thompson <mcthomps@us.ibm.com>
2018-09-11 20:18:00 -0500

    Add a /getenv and /properties to cf-spring-web

--- cf-spring-web/src/main/java/application/rest/GreetingController.java
+++ cf-spring-web/src/main/java/application/rest/GreetingController.java
@@ -1,6 +1,9 @@
 package application.rest;
 
+import java.util.Map;
+import java.util.Properties;
 import java.util.concurrent.atomic.AtomicLong;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,4 +18,14 @@ public class GreetingController {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
+
+    @RequestMapping("/getenv")
+	public Map<String,String> getenv() {
+		return System.getenv();
+	}
+
+    @RequestMapping("/properties")
+	public Properties getProperties() {
+		return System.getProperties();
+	}
 }
--- cf-spring-web/src/main/java/application/rest/HealthEndpoint.java
+++ cf-spring-web/src/main/java/application/rest/HealthEndpoint.java
@@ -9,11 +9,6 @@ public class HealthEndpoint implements HealthIndicator {
 
   @Override
   public Health health() {
-    /*
-    if (!healthy) {
-      return Health.down().withDetail("Not healthy", 500).build();
-    }
-    */
     return Health.up().build();
   }
 }
